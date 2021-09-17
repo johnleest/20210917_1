35d14e01e67bebe21b4b7bfb75883f00f3d9f8d8
johnleest <johnleest94@gmail.com>
2021-09-17 14:35:28 +0900

    수업내용 추가

--- 20210917.cpp
+++ 20210917.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-	int First = 0;
+	/*int First = 0;
 	int Second = 0;
 	
 	cout << "첫번째 숫자를 넣어주세요.";
@@ -14,6 +14,80 @@ int main()
 	cout << First << " + " << Second << " = ";
 	cout << First + Second;
 
+	return 0;*/
+
+	//int First = 0;
+	//int Second = 0;
+	//int Sum = 0;
+
+	//cin >> First;
+	//cin >> Second;
+	//
+
+	////이항
+	////+,-,*,/,%
+	////단항
+	////++,--
+	//
+	//Sum = First + Second;
+
+	//cout << Sum;
+	//
+	//return 0;
+
+	/*int A = 0;
+	int B = 0;
+	int C = 0;
+	int Sum = 0;
+
+	cout << "첫번째 과목 점수를 입력하세요.";
+	cin >> A;
+	cout << "두번째 과목 점수를 입력하세요.";
+	cin >> B;
+	cout << "세번째 과목 점수를 입력하세요.";
+	cin >> C;
+
+	cout << "세 과목의 합은 = ";
+
+	Sum = A + B + C;
+	cout << Sum;*/
+	
+	
+	//type
+	//int -> float
+	//float -> int
+	//float -> double 
+	//casting(형변환)
+	//(형)원래형태
+	//static_cast<새로운형>(원래형태)
+	//int -> char
+	//char -> int 
+	
+
+	/*int A = 0;
+	int B = 0;
+	int C = 0;
+	float D = 0.3f;
+
+	cout << "첫번째 과목 점수를 입력하세요.";
+	cin >> A;
+	cout << "두번째 과목 점수를 입력하세요.";
+	cin >> B;
+	cout << "세번째 과목 점수를 입력하세요.";
+	cin >> C;
+
+	cout << "세 과목의 평균은? ";
+	D = static_cast<float>((A + B + C)) / 3.0f;
+
+	cout << D;
+
+	return 0;*/
+
+	int A = 65;
+	char C = A;
+	
+	cout << C << endl;
+	
 	return 0;
 
 }
