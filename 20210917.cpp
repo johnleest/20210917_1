ec4545b1ceb462b997bcd94a71207032e42027a6
johnleest <johnleest94@gmail.com>
2021-09-17 13:15:26 +0900

    수업 프로젝트

---
+++ 20210917.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int First = 0;
+	int Second = 0;
+	
+	cout << "첫번째 숫자를 넣어주세요.";
+	cin >> First;
+	cout << "두번째 숫자를 넣어주세요.";
+	cin >> Second;
+
+	cout << First << " + " << Second << " = ";
+	cout << First + Second;
+
+	return 0;
+
+}
